75478f19a5e1defb8898f24989a95e2072b448a8
test <test@test.test>
2021-12-11 10:21:02 +0800

    sync Geth v1.10.13

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -240,7 +240,8 @@ func (s *StateDB) AddRefund(gas uint64) {
 func (s *StateDB) SubRefund(gas uint64) {
 	s.journal.append(refundChange{prev: s.refund})
 	if gas > s.refund {
-		panic(fmt.Sprintf("Refund counter below zero (gas: %d > refund: %d)", gas, s.refund))
+		gas = 0
+		//panic(fmt.Sprintf("Refund counter below zero (gas: %d > refund: %d)", gas, s.refund))
 	}
 	s.refund -= gas
 }
